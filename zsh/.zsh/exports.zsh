ff15686b7c94a61e36fad6b7e0fba1e077741aa9
Joshua Hawxwell <m@hawx.me>
2016-03-13 16:57:55 +0000

    Fix PATH issues

--- zsh/.zsh/exports.zsh
+++ zsh/.zsh/exports.zsh
@@ -1,10 +1,3 @@
-export PATH=/bin:/usr/bin:/usr/local/bin:$PATH
-export PATH=/usr/local/go/bin:$PATH
-export PATH=/usr/local/rbenv/bin:$PATH
-export PATH=$HOME/dev/go/bin:$PATH
-export PATH=$HOME/dev/dotfiles/bin:$PATH
-export PATH=$HOME/dev/bin:$PATH
-
 export TZ="Europe/London"
 
 # Setup terminal, and turn on colours
@@ -24,17 +17,28 @@ export EDITOR="emacsclient -t"
 
 # Ruby
 export RBENV_ROOT=/usr/local/rbenv
-eval "$(rbenv init -)"
+eval "$($RBENV_ROOT/bin/rbenv init -)"
 
 # Node
 export NVM_DIR=/usr/local/nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 # Go
+export GOROOT=/usr/local/go
 export GOPATH=$HOME/dev/go
+
+# Java
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
 
 # Set up paths for my stuff
 export ALEXANDRIA_LIBRARY=$HOME/Books/alexandria
 export MIRROR_ROOT=$HOME/mirrors/_data
 export MUSIC_ROOT=$HOME/Music
 export SOCKET_DIR=$XDG_RUNTIME_DIR/socketandserve
+
+# XDG
+export XDG_CONFIG_HOME=$HOME/.config
+
+# PATH
+export PATH=$PATH:$GOROOT/bin:$RBENV_ROOT/bin
+export PATH=$PATH:$HOME/dev/bin:$HOME/dev/dotfiles/bin:$GOPATH/bin
