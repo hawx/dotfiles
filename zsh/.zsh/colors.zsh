4766b3883e195e0363987a828c64a3fd09ff3e19
Joshua Hawxwell <m@hawx.me>
2018-12-29 17:18:14 +0000

    with hex for zsh colours

--- zsh/.zsh/colors.zsh
+++ zsh/.zsh/colors.zsh
@@ -24,25 +24,25 @@ export LS_COLORS=exfxcxdxbxegedabagacad
 
 # Colours for my ir_black mod are
 #
-# black: 0, 0, 0 
-# light black: 124, 124, 124 
-# red: 255, 108, 96 
-# light red: 255, 182, 176 
-# green: 168, 255, 96 
-# light green: 206, 255, 172 
-# yellow: 255, 255, 182 
-# light yellow: 255, 255, 204 
-# blue: 150, 203, 254 
-# light blue: 182, 220, 255 
-# magenta: 255, 115, 253 
-# light magenta: 255, 156, 254 
-# cyan: 198, 197, 254 
-# light cyan: 223, 223, 254 
-# white: 238, 238, 238 
-# light white: 255, 255, 255
+# black: 0, 0, 0 = #000000
+# light black: 124, 124, 124 = #7c7c7c
+# red: 255, 108, 96 = #ff6c60
+# light red: 255, 182, 176 = #ffb6b0
+# green: 168, 255, 96 = #a8ff60
+# light green: 206, 255, 172 = #ceffac
+# yellow: 255, 255, 182 = #ffffb6
+# light yellow: 255, 255, 204 = #ffffcc
+# blue: 150, 203, 254 = #96cbfe
+# light blue: 182, 220, 255 = #b6dcff
+# magenta: 255, 115, 253 = #ff73fd
+# light magenta: 255, 156, 254 = #ff9cfe
+# cyan: 198, 197, 254 = #c6c5fe
+# light cyan: 223, 223, 254 = #dfdffe
+# white: 238, 238, 238 = #eeeeee
+# light white: 255, 255, 255 = #ffffff
 # 
-# text: 242, 242, 242 
-# bold text: 255, 255, 255 
-# selection: 18, 19, 43 
-# cursor: 255, 165, 96 
-# background: 0, 0, 0
+# text: 242, 242, 242 = #f2f2f2
+# bold text: 255, 255, 255 = #ffffff
+# selection: 18, 19, 43 = #12132b
+# cursor: 255, 165, 96 = #ffa560
+# background: 0, 0, 0 = #000000
