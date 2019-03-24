ebde9a0c3aa87cced49bcc517adfd4e2b83ebe87
Joshua Hawxwell <m@hawx.me>
2019-03-24 13:32:45 +0000

    Remove zsh functions

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -3,21 +3,9 @@ function htmlize {
   pygmentize -f html -l $1
 }
 
-function git(){hub "$@"}
-
 function reset-permissions() {
   find . -type f -exec chmod 644 {} +;
   find . -type d -exec chmod 755 {} +;
-}
-
-function swap_emacs() {
-  mv ~/.emacs.d ~/.emacs.d.tmp
-  mv ~/.emacs.d.other ~/.emacs.d
-  mv ~/.emacs.d.tmp ~/.emacs.d.other
-}
-
-function uplogtail() {
-  tail -f $XDG_CACHE_HOME/upstart/$1.log
 }
 
 __git_files () {
