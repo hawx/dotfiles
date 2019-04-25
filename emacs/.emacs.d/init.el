a75cebd39c5e364effad0ff22f5f76500bb94145
Joshua Hawxwell <m@hawx.me>
2019-04-25 11:25:54 +0100

    Remove hydra-apropos

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -374,22 +374,6 @@ PROJECTILE: %(projectile-project-root)
 
 (use-package hydra
   :config
-  ;; https://github.com/abo-abo/hydra/blob/master/hydra-examples.el
-  (defhydra hydra-apropos (:color blue :hint nil)
-    "
-_a_propos        _c_ommand
-_d_ocumentation  _l_ibrary
-_v_ariable       _u_ser-option
-^ ^          valu_e_"
-    ("a" apropos)
-    ("d" apropos-documentation)
-    ("v" apropos-variable)
-    ("c" apropos-command)
-    ("l" apropos-library)
-    ("u" apropos-user-option)
-    ("e" apropos-value))
-  (global-set-key (kbd "C-c h") 'hydra-apropos/body)
-
   (defun mode-is-on (name)
     (and (boundp name)
          (or (eq (eval name) 't)
