e565cc8db5ecc68b1ba93eb0e62abcd4dbe08120
Joshua Hawxwell <m@hawx.me>
2019-05-08 10:26:08 +0100

    Add smerge hydra

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -232,6 +232,48 @@
   :config (setq magit-process-finish-apply-ansi-colors t)
   :bind ("C-x g" . magit-status))
 
+;; https://github.com/alphapapa/unpackaged.el#hydra
+(use-package smerge-mode
+  :after hydra
+  :bind ("C-x ^" . unpackaged/smerge-hydra/body)
+  :config
+  (defhydra unpackaged/smerge-hydra
+    (:color pink :hint nil :post (smerge-auto-leave))
+    "
+^Move^       ^Keep^               ^Diff^                 ^Other^
+^^-----------^^-------------------^^---------------------^^-------
+_n_ext       _b_ase               _<_: upper/base        _C_ombine
+_p_rev       _u_pper              _=_: upper/lower       _r_esolve
+^^           _l_ower              _>_: base/lower        _k_ill current
+^^           _a_ll                _R_efine
+^^           _RET_: current       _E_diff
+"
+    ("n" smerge-next)
+    ("p" smerge-prev)
+    ("b" smerge-keep-base)
+    ("u" smerge-keep-upper)
+    ("l" smerge-keep-lower)
+    ("a" smerge-keep-all)
+    ("RET" smerge-keep-current)
+    ("\C-m" smerge-keep-current)
+    ("<" smerge-diff-base-upper)
+    ("=" smerge-diff-upper-lower)
+    (">" smerge-diff-base-lower)
+    ("R" smerge-refine)
+    ("E" smerge-ediff)
+    ("C" smerge-combine-with-next)
+    ("r" smerge-resolve)
+    ("k" smerge-kill-current)
+    ("ZZ" (lambda ()
+            (interactive)
+            (save-buffer)
+            (bury-buffer))
+     "Save and bury buffer" :color blue)
+    ("q" nil "cancel" :color blue))
+  :hook (magit-diff-visit-file . (lambda ()
+                                   (when smerge-mode
+                                     (unpackaged/smerge-hydra/body)))))
+
 (use-package deadgrep
   :bind ("C-c s" . deadgrep))
 
@@ -585,8 +627,7 @@ _w_ whitespace-mode        %(mode-is-on 'whitespace-mode)
 (defun create-scratch-buffer nil
    "create a scratch buffer"
    (interactive)
-   (switch-to-buffer (get-buffer-create "*scratch*"))
-   (lisp-interaction-mode))
+   (switch-to-buffer (get-buffer-create "*scratch*")))
 
 (windmove-default-keybindings)
 (global-set-key (kbd "C-c TAB") 'switch-to-previous-buffer)
